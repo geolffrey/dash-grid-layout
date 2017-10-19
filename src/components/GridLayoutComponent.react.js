6b976df5acea5c1f65764443fba2351429d256d3
Lewis Dale <lewis@lewisdale.co.uk>
2017-10-19 11:51:02 +0100

    Added bindings for React Grid Layout props

--- src/components/GridLayoutComponent.react.js
+++ src/components/GridLayoutComponent.react.js
@@ -5,15 +5,35 @@ import ReactGridLayout from 'react-grid-layout';
 class GridLayoutComponent extends Component {
   render() {
     const {
-      rowHeight = 30,
-      cols = 12,
-      width = 1200, // May remove, make responsive
-      children,
+      children
     } = this.props;
 
     return (
-      <ReactGridLayout width={width} rowHeight={rowHeight} cols={cols}>
-        { children }
+      <ReactGridLayout
+        width={this.props.width}
+        rowHeight={this.props.rowHeight}
+        cols={this.props.cols}
+        autoSize={this.props.autoSize}
+        draggableCancel={this.props.draggableCancel}
+        draggableHandle={this.props.draggableHandle}
+        verticalCompact={this.props.verticalCompact}
+        compactType={this.props.compactType}
+        layout={this.props.layout}
+        margin={this.props.margin}
+        containerPadding={this.props.containerPadding}
+        isDraggable={this.props.isDraggable}
+        isResizable={this.props.isResizable}
+        useCSSTransforms={this.props.useCSSTransforms}
+        preventCollision={this.props.preventCollision}
+        onLayoutChange={this.props.onLayoutChange}
+        onDragStart={this.props.onDragStart}
+        onDrag={this.props.onDrag}
+        onDragStop={this.props.onDragStop}
+        onResizeStart={this.props.onResizeStart}
+        onResize={this.props.onResize}
+        onResizeStop={this.props.onResizeStop}
+      >
+      { children }
       </ReactGridLayout>
     );
   }
@@ -46,9 +66,133 @@ GridLayoutComponent.propTypes = {
   width: PropTypes.number,
 
   /**
+   * If true, containers will automatically resize to fit the content
+   */
+  autoSize: PropTypes.bool,
+
+  /**
+   * CSS selector for tags that will not be draggable. Requires a leading '.'
+   */
+  draggableCancel: PropTypes.string,
+
+  /**
+   * CSS selector for tags that will act as the draggable handle. Requires a leading '.'
+   */
+  draggableHandle: PropTypes.string,
+
+  /**
+   * If true, the layout will compact vertically
+   */
+  verticalCompact: PropTypes.bool,
+
+  /**
+   * Compaction type.
+   * One of 'vertical' and 'horizontal'
+   */
+  compactType: PropTypes.oneOf(['vertical', 'horizontal']),
+
+  /**
+   * Array of objects with the format:
+   * { x: number, y: number, w: number, h: number }
+   * If custom keys are used, then an optional `i` parameter can
+   * be added that matches the key
+   */
+  layout: PropTypes.arrayOf(PropTypes.shape({
+    x: PropTypes.number.isRequired,
+    y: PropTypes.number.isRequired,
+    w: PropTypes.number.isRequired,
+    h: PropTypes.number.isRequired,
+    i: PropTypes.Number
+  })),
+
+  /**
+   * Margin between items [x, y] in px
+   */
+  margin: PropTypes.arrayOf(PropTypes.number),
+
+  /**
+   * Padding inside the container [x, y] in px
+   */
+  containerPadding: PropTypes.arrayOf(PropTypes.number),
+
+  /**
+   * Elements can be dragged
+   */
+  isDraggable: PropTypes.bool,
+
+  /**
+   * Elements can be resized
+   */
+  isResizable: PropTypes.bool,
+
+  /**
+   * Use CSS transforms instead of Position. Improves performance if switched on
+   */
+  useCSSTransforms: PropTypes.bool,
+
+  /**
+   * If true, grid items won't change position when being
+   * dragged over
+   */
+  preventCollision: PropTypes.bool,
+
+  /**
+   * Callback upon the layout changed
+   * @param layout: the layout
+   */
+  onLayoutChange: PropTypes.func,
+
+  /**
+   * Callback when dragging is started
+   */
+  onDragStart: PropTypes.func,
+
+  /**
+   * Callback upon each drag movement
+   */
+  onDrag: PropTypes.func,
+
+  /**
+   * Callback upon drag completion
+   */
+  onDragStop: PropTypes.func,
+
+  /**
+   * Calls when resize starts
+   */
+  onResizeStart: PropTypes.func,
+
+  /**
+   * Calls when resize movement happens
+   */
+  onResize: PropTypes.func,
+
+  /**
+   * Calls when resize is complete
+   */
+  onResizeStop: PropTypes.func,
+
+  /**
    * Dash-assigned callback that should be called whenever any of the properties change
    */
   setProps: PropTypes.func
+};
+
+GridLayoutComponent.defaultProps = {
+  width: 1200,
+  autoSize: true,
+  cols: 12,
+  draggableCancel: '',
+  draggableHandle: '',
+  verticalCompact: true,
+  compactType: 'vertical',
+  margin: [10, 10],
+  containerPadding: [10,10],
+  rowHeight: 150,
+  isDraggable: true,
+  isResizable: true,
+  useCSSTransforms: true,
+  preventCollision: false
 };
 
 export default GridLayoutComponent;
