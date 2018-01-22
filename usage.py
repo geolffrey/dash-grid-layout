95f43ad2e3beef41e7516373aebae7e04cf91202
Lewis Dale <lewis.dale@fortacode.co.uk>
2018-01-22 14:39:03 +0000

    Updated usage.py, removed references to absent dependency

--- usage.py
+++ usage.py
@@ -3,34 +3,10 @@ import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_widget_component as dwc
 
-filters=[
-    {
-        'name': 'filter1',
-        'values': [
-            'value1',
-            'value2',
-            'value3',
-        ],
-        'default': 'value1'
-    },
-    {
-        'name': 'filter2',
-        'values': [
-            'value4',
-            'value5',
-            'value6',
-        ],
-        'default': 'value4'
-    },
-]
 app = dash.Dash('')
 
 app.scripts.config.serve_locally = True
-app.css.append_css({
-    "external_url": "https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"
-})
 
 app.layout = html.Div([
     dgl.GridLayout(
@@ -64,8 +40,7 @@ app.layout = html.Div([
                         config={
                             'autosizable': True
                             }
-                        ),
-                        dwc.Widget(type='global',filters=filters)
+                        )
                     ],
                 i='2',
                 layout={
@@ -93,9 +68,7 @@ app.layout = html.Div([
                             'doubleClick': 'autosize',
                             'frameMargins': 0,
                             },
-                        ),
-                    html.Div('Hello World')
-
+                        )
                     ],
                 i='3',
                 layout={
